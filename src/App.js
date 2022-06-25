b781c6bad2ed46555e0fe5f03a11f8fb7ab7bb9f
TrietB <triet.bm1996@gmail.com>
2022-06-25 14:51:05 +0700

    1st

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState} from 'react'
+import './App.css'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    const [tasks, setTasks] = useState([
+        {id: 1, title: "Learn JS", status: 0},
+        {id: 2, title: "Code a Todo List", status: 1},
+    ])
+    const [showIncomplete, setShowIncomplete] = useState(true)
+    
+    const [newTask, setNewTask] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if(newTask){
+            const task =  {id: Date.now(), title: newTask, status: 0}
+            setTasks([...tasks, task])
+            setNewTask('')
+        }
+
+    }
+    const setTaskStatus = (taskId, status) => {
+        setTasks(
+            tasks.map((task)=>{
+                if(task.id === taskId){
+                    return {...task, status: status ? 1 : 0}
+                }
+                return task
+            })
+        )
+    }
+
+    const removeTask = (taskId) => {
+        setTasks(tasks.filter((task)=> task.id !== taskId))
+    }
+
+    const handleInputChange = (e) => {
+        setNewTask(e.target.value)
+    }
+    return (
+    <div className="container">
+    <h1 className="title">
+      Todo List
+      <span>Get things done, one item at a time.</span>
+    </h1>
+    <ul className="task-list">
+        {tasks.filter((task) => showIncomplete ? task.status !==1 : true ).map((task)=> (
+            <li key={task.id} className={task.status ? 'done' : ''}>
+            <span className="label">{task.title}</span>
+            <div className="actions">
+              <input type="checkbox" className="btn-action btn-action-done" checked={task.status}
+              onChange={(e)=> setTaskStatus(task.id, e.target.checked)} />
+              <button className="btn-action btn-action-delete" onClick={() => removeTask(task.id)}>✖</button>
+            </div>
+          </li>
+        ))}
+    </ul>
+    <div className="filter-wrapper">
+      <label htmlFor="filter" className="filter-label">
+        Show incompleted tasks only
+      </label>
+      <input type="checkbox" id="filter" checked={showIncomplete} onChange={(e)=>setShowIncomplete(e.target.checked)} />
     </div>
-  );
+    <form action="#" className="htmlForm" onSubmit={handleSubmit}>
+      <label htmlFor="newitem">Add to the todo list</label>
+      <input type="text" id="newitem" value={newTask} onChange={handleInputChange}/>
+      <button type="submit">Add Item</button>
+    </form>
+  </div>
+  )
 }
 
-export default App;
+export default App
